6cd25aa11b13c052ea29044edeea0cd5c5d401ff
信鑫-King <45808948@qq.com>
2017-04-23 07:30:33 +0800

    add: redux

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 
-import './less/index.less';
+import { counter } from './reducer'
+import './less/index.less'
+
+import Counter from './components/Counter'
+
+
+const store = createStore(counter)
 
 class App extends Component {
     render () {
-      
         return (
           <div>
-          Hello React Markdown
-          <h1>DEV Mode: </h1>
-          <code>npm dev</code> 
-
-          <h1>Production Mode: </h1>
-          <code>npm start</code> 
-
+            <Counter 
+            value={store.getState()} 
+            onIncrement={() => 
+              store.dispatch({ type: 'INCREMENT' })
+            }
+            onDecrement={() => 
+              store.dispatch({ type: 'DECREMENT' })
+            }        
+          />
           </div>
         );
     }
 }
 
-ReactDOM.render(
+const render = () => {
+  ReactDOM.render(
   (
     <App />
   ),
   document.getElementById('app')
-);
+)};
+
+store.subscribe(render)
+render()
